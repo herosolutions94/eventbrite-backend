b23c9b232efbcf6466617b63fa3e5e4450f3ad24
Naeem <naeem@herosolutions.com.pk>
2024-01-15 19:19:50 +0500

    Project upload

---
+++ app/Models/TournamentRounds.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TournamentRounds extends Model
+{
+    use HasFactory;
+    protected $table = 'tournament_rounds';
+    protected $fillable = [
+        'tournament_id',
+        'user_id',
+        'type',
+        'round_no',
+        'status',
+    ];
+
+    public function tournament()
+    {
+        return $this->belongsTo(Tournament::class);
+    }
+    public function matches(){
+        return $this->hasMany(TournamentMatches::class,'round_id','id');
+    }
+}
